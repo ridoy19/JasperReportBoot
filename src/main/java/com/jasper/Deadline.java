4d1d735d51bd55b46d8f96212ee0e17e3734da09
ridoy19 <jh_ridoy@yahoo.com>
2023-06-11 07:28:51 +0600

    column diff working

--- src/main/java/com/jasper/Deadline.java
+++ src/main/java/com/jasper/Deadline.java
@@ -1,5 +1,6 @@
 package com.jasper;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -16,63 +17,110 @@ import java.util.HashMap;
  * @since 2018/1/24
  */
 public class Deadline {
-	//Now Date
-	LocalDateTime nowdate;
-	// Start Date
-	LocalDate startdate;
-	// End Date
-	LocalDate enddate;
-	
-	/**
-	 * Constructor
-	 * 
-	 * get current date.
-	 * 
-	 * generated started date and closing date for invoice.
-	 * 
-	 */
-	public Deadline(){
-		// Start Date
-		nowdate=LocalDateTime.now();
-		startdate=LocalDate.now();
-		startdate=startdate.minusMonths(1);
-		startdate=LocalDate.of(startdate.getYear(), startdate.getMonth(),26);
-		// End Date
-		enddate=LocalDate.now();
-		enddate=LocalDate.of(enddate.getYear(), enddate.getMonth(), 25);
-	}
-	
-	public String startdate(){
-		return startdate.toString();
-	}
-	
-	public String enddate(){
-		return enddate.toString();
-	}
-	
-	/**
-	 * getParameter Method
-	 * 
-	 * get generated date packed in HashMap.
-	 * 
-	 * this date change java.sql.date.
-	 * JasperReports don't deal java.time and can deal java.sql.date.
-	 * 
-	 * @return parameters  startdate and enddate 
-	 */
-	public HashMap<String, Object> getParameter(){
-		HashMap<String,Object> parameters =new HashMap<>();
-		Date sdate=Date.valueOf(startdate);
-		Date edate=Date.valueOf(enddate);
-		
-        parameters.put("startdate",sdate);
-        parameters.put("enddate",edate);
-               
-        return parameters;
-	}
-	
-	public LocalDateTime getNowdate(){
-		return this.nowdate;
+//	//Now Date
+//	LocalDateTime nowdate;
+//	// Start Date
+//	LocalDate startdate;
+//	// End Date
+//	LocalDate enddate;
+//
+//	/**
+//	 * Constructor
+//	 *
+//	 * get current date.
+//	 *
+//	 * generated started date and closing date for invoice.
+//	 *
+//	 */
+//	public Deadline(){
+//		// Start Date
+//		nowdate=LocalDateTime.now();
+//		startdate=LocalDate.now();
+//		startdate=startdate.minusMonths(1);
+//		startdate=LocalDate.of(startdate.getYear(), startdate.getMonth(),26);
+//		// End Date
+//		enddate=LocalDate.now();
+//		enddate=LocalDate.of(enddate.getYear(), enddate.getMonth(), 25);
+//	}
+////
+////	public String startdate(){
+////		return startdate.toString();
+////	}
+////
+////	public String enddate(){
+////		return enddate.toString();
+////	}
+//
+//	public void setNowdate(LocalDateTime nowdate) {
+//		this.nowdate = nowdate;
+//	}
+//
+//	public LocalDate getStartdate() {
+//		return startdate;
+//	}
+//
+//	public void setStartdate(LocalDate startdate) {
+//		this.startdate = startdate;
+//	}
+//
+//	public LocalDate getEnddate() {
+//		return enddate;
+//	}
+//
+//	public void setEnddate(LocalDate enddate) {
+//		this.enddate = enddate;
+//	}
+//
+//	/**
+//	 * getParameter Method
+//	 *
+//	 * get generated date packed in HashMap.
+//	 *
+//	 * this date change java.sql.date.
+//	 * JasperReports don't deal java.time and can deal java.sql.date.
+//	 *
+//	 * @return parameters  startdate and enddate
+//	 */
+//	public HashMap<String, Object> getParameter(){
+//		HashMap<String,Object> parameters =new HashMap<>();
+//		Date sdate=Date.valueOf(startdate);
+//		Date edate=Date.valueOf(enddate);
+//
+//        parameters.put("startdate",sdate);
+//        parameters.put("enddate",edate);
+//
+//        return parameters;
+//	}
+//
+//	public LocalDateTime getNowdate(){
+//		return this.nowdate;
+//	}
+
+	private LocalDateTime nowDte;
+	private LocalDate startDate;
+	private LocalDate endDate;
+
+	public LocalDateTime getNowDte() {
+		return nowDte;
 	}
 
+	public void setNowDte(LocalDateTime nowDte) {
+		this.nowDte = nowDte;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
 }
--- src/main/java/com/jasper/pdfController.java
+++ src/main/java/com/jasper/pdfController.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -72,29 +76,35 @@ public class pdfController {
 	@ResponseBody
     public void getPdf(@PathVariable String jrxml,@RequestParam("no") Optional<Integer> no,
       @RequestParam("date") Optional<Date> date, HttpServletResponse response) throws IOException, JRException {
-		    Resource resource = context.getResource("classpath:jasperreports/" + jrxml + (no.isPresent() ? no.get() : "") +".jrxml");
-        //Compile to jasperReport
-        InputStream inputStream = resource.getInputStream();
-        JasperReport report=JasperCompileManager.compileReport(inputStream);		
+    // Load the report template
+    Resource resource = context.getResource("classpath:jasperreports/" + jrxml + (no.isPresent() ? no.get() : "") + ".jrxml");
+    InputStream inputStream = resource.getInputStream();
+    JasperReport report = JasperCompileManager.compileReport(inputStream);
 
-        //Parameters Set
-        Map<String, Object> params;
-        Deadline deadline=new Deadline();
-        params=deadline.getParameter();
-        params.put("no", no.orElse(null));
-        params.put("date", date.orElse(null));
-        
-        //LOG
-        LOG.info("Template is [{}]. params is [{}] date is [{}]", jrxml,params,deadline.nowdate);
+// Create data source
+    Deadline deadline = new Deadline();
+    deadline.setStartDate(LocalDate.now());
+    deadline.setEndDate(LocalDate.now().plusDays(3));
 
-        
-        //Data source Set
-        JRDataSource dataSource = new JREmptyDataSource();
-        //Make jasperPrint
-        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
-        //Media Type
-        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
-        //Export PDF Stream
-        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
-    }
+    List<Deadline> list = new ArrayList<>();
+    list.add(deadline);
+    JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(list);
+
+// Set parameters
+    Map<String, Object> params = new HashMap<>();
+    params.put("no", no.orElse(null));
+    params.put("date", date.orElse(null));
+    params.put("dataSource", beanCollectionDataSource);
+    params.put("startDate", Date.valueOf(deadline.getStartDate()));
+    params.put("endDate", Date.valueOf(deadline.getEndDate()));
+
+// Log information
+//    LOG.info("Template is [{}]. Params are [{}]. Date is [{}]", jrxml, params, deadline.nowdate);
+    JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, beanCollectionDataSource);
+    // Export the report to PDF and send it as response
+    JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+
+    // Set response content type
+    response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+  }
 }
